01f10e72e381307bb6ad34b219c4335639bb7a7b
Ersi Ni <ersi.ni@nilbot.net>
2016-11-12 20:38:28 +0000

    fix log redirect

--- app/hackernews/restart.sh
+++ app/hackernews/restart.sh
@@ -2,5 +2,5 @@
 
 go get -u -v github.com/nilbot/slackbot/app/hackernews
 kill -9 $(ps ux | grep '[h]ackernews' | awk '{print $2}')
-hackernew $(cat ~/.slackbot) 2>&1 > ~/.hackernews.log &
+hackernew $(cat ~/.slackbot) >> ~/.hackernews.log 2>&1 &
 disown
