fc8fb5960c1456c6508d0ae2e97eb5d473ecfdfe
Ersi Ni <ersi.ni@nilbot.net>
2016-11-12 20:44:02 +0000

    fix exe...

--- app/hackernews/restart.sh
+++ app/hackernews/restart.sh
@@ -3,5 +3,5 @@
 go get -u -v github.com/nilbot/slackbot/app/hackernews
 kill -9 $(ps ux | grep '[h]ackernews' | awk '{print $2}')
 token=$(cat ~/.slackbot)
-hackernew "$token" >> ~/.hackernews.log 2>&1 &
+hackernews "$token" >> ~/.hackernews.log 2>&1 &
 disown
