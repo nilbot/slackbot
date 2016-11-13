c438b212a6abba258490534a39d4dbaf8e938ad6
Ersi Ni <ersi.ni@nilbot.net>
2016-11-13 04:05:52 +0000

    use cred

--- app/hackernews/restart.sh
+++ app/hackernews/restart.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 go get -u -v github.com/nilbot/slackbot/app/hackernews
+export GOOGLE_APPLICATION_CREDENTIALS=~/cred.json
 kill -9 $(ps ux | grep '[h]ackernews ' | awk '{print $2}')
 token=$(cat ~/.slackbot)
 hackernews "$token" >> ~/.hackernews.log 2>&1 &
